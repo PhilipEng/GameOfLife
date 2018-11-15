e21a9565967cce2915cd61433d2f047c64a100ee
Jamie.Beattie <jamie.beattie@ucdconnect.ie>
2018-11-15 17:02:38 +0000

    Adding new spaces class

---
+++ GameOfLife/src/gameoflife/board/spaces/SpaceType.java
@@ -0,0 +1,20 @@
+package gameoflife.board.spaces;
+
+public enum SpaceType {
+	START_CAREER,
+	START_COLLEGE,
+	PAYDAY,
+	ACTION,
+	HOLIDAY,
+	SPIN_TO_WIN,
+	BABY,
+	TWINS,
+	HOUSE,
+	STOP_GRADUATE,
+	STOP_MARRIAGE,
+	STOP_SCHOOL,
+	STOP_FAMILY,
+	STOP_BABY,
+	STOP_HOLIDAY,
+	RETIRE;
+}
